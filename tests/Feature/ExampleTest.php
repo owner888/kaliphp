8298df213d710b9accde869ec2ebd74398ecc2c6
seatle888 <seatle888@gmail.com>
2024-04-14 14:26:26 +0700

    feat(test): 跳过测试, test 简写 it

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,10 +1,10 @@
 <?php
 
-test('example', function () {
+it('example', function () {
     expect(true)->toBeTrue();
     expect(1)->toBeInt();
     expect(1.0)->toBeFloat();
     expect("test")->toBeString();
     expect("111")->toBeNumeric();
     expect(["111", "222"])->toBeArray();
-});
+})->skip("Test only");
