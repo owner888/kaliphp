c5a44d311c20ba69ab8293fdd3fcc058f370b614
seatle888 <seatle888@gmail.com>
2024-04-14 14:16:18 +0700

    feat(test): 增加几个常用类型预期测试

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,4 +2,9 @@
 
 test('example', function () {
     expect(true)->toBeTrue();
+    expect(1)->toBeInt();
+    expect(1.0)->toBeFloat();
+    expect("test")->toBeString();
+    expect("111")->toBeNumeric();
+    expect(["111", "222"])->toBeArray();
 });
